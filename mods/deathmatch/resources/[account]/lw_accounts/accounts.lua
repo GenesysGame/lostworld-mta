5e23a0c1e036906a6368fa4e8d1705b5d0b270d9
BozhkoAlexander <bozhko.inosoft@gmail.com>
2016-04-01 00:42:13 +0300

    increased fps limit; added lw main resource; added lw_accounts resource.

---
+++ mods/deathmatch/resources/[account]/lw_accounts/accounts.lua
@@ -0,0 +1,13 @@
+-- Server file for accounts management
+
+
+--TODO: Delete ------------------------------------------------
+local spawnX, spawnY, spawnZ = 1959.55, -1714.46, 12
+function joinHandler()
+	spawnPlayer(source, spawnX, spawnY, spawnZ)
+	fadeCamera(source, true)
+	setCameraTarget(source, source)
+	outputChatBox("Welcome to My Server", source)
+end
+addEventHandler("onPlayerJoin", getRootElement(), joinHandler)
+---------------------------------------------------------------
