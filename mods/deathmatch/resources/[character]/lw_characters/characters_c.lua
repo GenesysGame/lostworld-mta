230d5ac1cdcd3d6cac98f456bfc140580fc3c558
BozhkoAlexander <bozhko.inosoft@gmail.com>
2016-08-27 16:06:07 +0300

    updated character states window.

--- mods/deathmatch/resources/[character]/lw_characters/characters_c.lua
+++ mods/deathmatch/resources/[character]/lw_characters/characters_c.lua
@@ -6,6 +6,7 @@ local statesWindow = nil
 addEvent("client:onCharacterUpdated", true)
 
 function keyPressed(button, press)
+	if not localPlayer:getData("charModel") then return end
     if press and button == "i" then
     	local show = not statesWindow.wdw.visible
     	if show then
@@ -43,45 +44,45 @@ addEventHandler("client:onCharacterUpdated", localPlayer, updateStatesWindow)
 function createStatesWindow( )
 	local x = 20
 	local rX = 170
-	local y = 0.5 * scrH - 105
+	local y = 0.5 * scrH - 135
 	local width = 300
-	local height = 300
+	local height = 270
 	local aTable = {}
-	aTable.wdw = guiCreateWindow(x, y, width, height, "States Window v0.01", false)
+	aTable.wdw = guiCreateWindow(x, y, width, height, "Характеристики персонажа v0.02", false)
 
 	x = 20
 	y = 30
 	width = 130
 	height = 20
-	guiCreateLabel(x, y, width, height, "Firstname:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Имя:", false, aTable.wdw)
 	aTable.lblFirstname = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw)
 
 	y = 60
-	guiCreateLabel(x, y, width, height, "Lastname:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Фамилия:", false, aTable.wdw)
 	aTable.lblLastname = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw)
 
 	y = 90
-	guiCreateLabel(x, y, width, height, "Sex:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Пол:", false, aTable.wdw)
 	aTable.lblSex = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw)
 
 	y = 120
-	guiCreateLabel(x, y, width, height, "Race:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Раса:", false, aTable.wdw)
 	aTable.lblRace = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw)
 
 	y = 150
-	guiCreateLabel(x, y, width, height, "Age:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Возраст:", false, aTable.wdw)
 	aTable.lblAge = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw)
 
 	y = 180
-	guiCreateLabel(x, y, width, height, "Experience:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Жизненный опыт:", false, aTable.wdw)
 	aTable.lblExp = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw) 
 
 	y = 210
-	guiCreateLabel(x, y, width, height, "Volume:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Объем инвентаря:", false, aTable.wdw)
 	aTable.lblvol = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw) 
 
 	y = 240
-	guiCreateLabel(x, y, width, height, "Weight:", false, aTable.wdw)
+	guiCreateLabel(x, y, width, height, "Грузоподъемность:", false, aTable.wdw)
 	aTable.lblwei = guiCreateLabel(rX, y, width, height, "", false, aTable.wdw) 
 
 	-- делаем окно невидимым
